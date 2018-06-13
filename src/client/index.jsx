369c22f5aa2753cd8254208be25934e0329dc2b4
talarKoudoussian <talar.koudoussian@gmail.com>
2018-06-13 10:42:53 +0300

    feat: add hot module replacement

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -4,14 +4,32 @@ import 'babel-polyfill'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
 import App from './app'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR)
 
+const wrapApp = AppComponent =>
+    (
+        <AppContainer>
+            <AppComponent />
+        </AppContainer>
+    )
+
 if (!(rootEl instanceof Element)) {
     throw new Error('invalid type')
 }
 
-ReactDOM.render(<App />, rootEl)
+ReactDOM.render(wrapApp(App), rootEl)
+
+// flow-disable-next-line
+if (module.hot) {
+    // flow-disable-next-line
+    module.hot.accept('./app', () => {
+        // eslint-disable-next-line global-require
+        const NextApp = require('./app').default
+        ReactDOM.render(wrapApp(NextApp), rootEl)
+    })
+}
